9672e89316b9e45db5d4214a81100adbad7a77cb
Levis Chiri <120241319+ChiriKamau@users.noreply.github.com>
2023-04-08 22:12:20 +0300

    Add files via upload
    The program can be used with arduino to detect  an object using  IR sensor and display intruder if their is an object on liquid crystal display (LCD).

---
+++ intruder.ino
@@ -0,0 +1,32 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int IRsensor = 1;
+
+void setup()
+{
+	// initialize the LCD
+lcd.begin();
+pinMode(IRsensor,INPUT);	
+// pinMode(LED,OUTPUT);
+
+}
+
+void loop()
+{
+digitalRead(IRsensor);
+int val=digitalRead(IRsensor);
+if(val==0){
+// digitalWrite(LED,HIGH);
+lcd.setCursor(2,0);   
+	lcd.print("Intruder");
+}
+else{
+// digitalWrite(LED,LOW);
+lcd.setCursor(2,0);
+	lcd.print("NO Intruder");  
+}
+
+}
